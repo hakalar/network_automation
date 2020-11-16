e55e1bae50e5ca9c384d990fe494c65f424c3544
roberthakala <840717Rh!>
2020-11-17 00:43:00 +0800

    Added 3 netmiko scripts for SSH to a Cisco router

---
+++ _python/003 ssh to multiple routers defined in module/device_info.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+cisco_csr1kv_1 = {
+    'device_type': 'cisco_ios',
+    'host':   '192.168.57.3',
+    'username': 'cisco',
+    'password': 'cisco', # get password from user input from cli
+    'port' : 22,          # optional, defaults to 22
+    'secret': 'cisco',     # optional, defaults to ''
+}
+
+cisco_csr1kv_2 = {
+    'device_type': 'cisco_ios',
+    'host':   '192.168.57.4',
+    'username': 'cisco',
+    'password': 'cisco',  # static ssh password
+    'port' : 22,          # optional, defaults to 22
+    'secret': 'cisco',     # optional, defaults to ''
+}
